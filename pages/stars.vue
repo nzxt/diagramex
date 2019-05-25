7184287edeef6212a031596bc6609e015e12e313
Deniz True <nzxt.ua@gmail.com>
2019-05-25 16:16:50 +0300

    Stars page added

--- pages/stars.vue
+++ pages/stars.vue
@@ -1,23 +1,22 @@
-<template>
-  <div>
-    <v-stage
-      ref="stage"
-      :config="configKonva"
-      @dragstart="handleDragstart"
-      @dragend="handleDragend"
-    >
-      <v-layer ref="layer">
-        <v-star v-for="item in list" :key="item.id" :config="item"></v-star>
-      </v-layer>
-      <v-layer ref="dragLayer"></v-layer>
-    </v-stage>
-  </div>
+<template lang="pug">
+  div
+    v-stage(
+      ref='stage'
+      :config='configKonva'
+      @dragstart='handleDragstart'
+      @dragend='handleDragend'
+    )
+      v-layer(ref='layer')
+        v-star.pointer(v-for='item in list' :key='item.id' :config='item')
+      v-layer(ref='dragLayer')
 </template>
 
 <script>
-const width = window.innerWidth;
-const height = window.innerHeight;
-let vm = {};
+import Konva from 'konva'
+
+const width = window.innerWidth
+const height = window.innerHeight
+
 export default {
   data() {
     return {
@@ -26,64 +25,71 @@ export default {
         width: width,
         height: height
       }
-    };
-  },
-  methods: {
-    handleDragstart(e) {
-      const shape = e.target;
-      const dragLayer = vm.$refs.dragLayer.getNode();
-      const stage = vm.$refs.stage.getNode();
-      // moving to another layer will improve dragging performance
-      shape.moveTo(dragLayer);
-      stage.draw();
-      shape.setAttrs({
-        shadowOffsetX: 15,
-        shadowOffsetY: 15,
-        scaleX: shape.getAttr("startScale") * 1.2,
-        scaleY: shape.getAttr("startScale") * 1.2
-      });
-    },
-    handleDragend(e) {
-      const shape = e.target;
-      const layer = vm.$refs.layer.getNode();
-      const stage = vm.$refs.stage.getNode();
-      shape.moveTo(layer);
-      stage.draw();
-      shape.to({
-        duration: 0.5,
-        easing: Konva.Easings.ElasticEaseOut,
-        scaleX: shape.getAttr("startScale"),
-        scaleY: shape.getAttr("startScale"),
-        shadowOffsetX: 5,
-        shadowOffsetY: 5
-      });
     }
   },
+
   mounted() {
-    vm = this;
     for (let n = 0; n < 30; n++) {
-      const scale = Math.random();
-      const stage = vm.$refs.stage.getStage();
-      vm.list.push({
+      const scale = Math.random()
+      const stage = this.$refs.stage.getStage()
+      this.list.push({
         x: Math.random() * stage.width(),
         y: Math.random() * stage.height(),
         rotation: Math.random() * 180,
         numPoints: 5,
         innerRadius: 30,
         outerRadius: 50,
-        fill: "#89b717",
+        fill: '#89b717',
         opacity: 0.8,
         draggable: true,
         scaleX: scale,
         scaleY: scale,
-        shadowColor: "black",
+        shadowColor: 'black',
         shadowBlur: 10,
         shadowOffsetX: 5,
         shadowOffsetY: 5,
         shadowOpacity: 0.6,
         startScale: scale
-      });
+      })
+    }
+  },
+
+  methods: {
+    handleDragstart(e) {
+      const shape = e.target
+      const dragLayer = this.$refs.dragLayer.getNode()
+      const stage = this.$refs.stage.getNode()
+      // moving to another layer will improve dragging performance
+      shape.moveTo(dragLayer)
+      stage.draw()
+      shape.setAttrs({
+        shadowOffsetX: 15,
+        shadowOffsetY: 15,
+        scaleX: shape.getAttr('startScale') * 1.2,
+        scaleY: shape.getAttr('startScale') * 1.2
+      })
+    },
+
+    handleDragend(e) {
+      const shape = e.target
+      const layer = this.$refs.layer.getNode()
+      const stage = this.$refs.stage.getNode()
+      shape.moveTo(layer)
+      stage.draw()
+      shape.to({
+        duration: 0.5,
+        easing: Konva.Easings.ElasticEaseOut,
+        scaleX: shape.getAttr('startScale'),
+        scaleY: shape.getAttr('startScale'),
+        shadowOffsetX: 5,
+        shadowOffsetY: 5
+      })
     }
   }
-};
+}
 </script>
+
+<style lang="stylus" scoped>
+  .pointer
+    cursor pointer
+</style>
