947c01e7678d2bc372f832515e0bdee977ab723f
Deniz True <nzxt.ua@gmail.com>
2019-05-26 23:24:39 +0300

    Index Page + UseCase

---
+++ components/UseCase.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+  v-group(:config='groupConfig')
+    v-text(:config='textConfig')
+    v-rect(:config='titleConfig')
+    v-rect(:config='bodyConfig')
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class UseCase extends Vue {
+  groupConfig: any = {
+    x: 0,
+    y: 0,
+    draggable: true
+  }
+  textConfig: any = {
+    x: 0,
+    y: 0,
+    text: 'COMPLEX TEXT',
+    fontSize: 14,
+    fontFamily: 'Calibri',
+    fill: '#fff',
+    // width: 100,
+    padding: 10,
+    align: 'left',
+    zIndex: 3
+  }
+  titleConfig: any = {
+    x: 0,
+    y: 0,
+    stroke: '#555',
+    strokeWidth: 2,
+    fill: '#2BC30A',
+    width: 300,
+    height: 35,
+    shadowColor: 'black',
+    shadowBlur: 10,
+    shadowOffset: [10, 10],
+    shadowOpacity: 0.2,
+    cornerRadius: 5,
+    opacity: 0.75,
+    zIndex: 1
+  }
+  bodyConfig: any = {
+    x: 0,
+    y: 30,
+    stroke: '#555',
+    strokeWidth: 2,
+    fill: '#6FBAE6',
+    width: 400,
+    height: 200,
+    shadowColor: 'black',
+    shadowBlur: 10,
+    shadowOffset: [10, 10],
+    shadowOpacity: 0.2,
+    cornerRadius: 5,
+    // opacity: 0.75,
+    zIndex: 2
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,140 @@
-<template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >discord</a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >issue board</a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+<template lang='pug'>
+  v-layout.justify-center
+    v-flex.xs12
+      v-card#editor.fill-height(v-resize='calcStageSize')
+        v-card-title.py-1.title.grey.white--text.font-weight-thin
+          | Viete.io
+          | from [ {{ dragStart.x }}, {{ dragStart.y }} ] to [ {{ dragEnd.x }}, {{ dragEnd.y }} ]
+          v-spacer
+          v-chip(dark small) {{ scaleConfig.scaleX.toFixed(2) }} * {{ scaleConfig.scaleY.toFixed(2) }}
+          v-spacer
+          v-btn(icon small @click='scaleIn')
+            v-icon.mdi-24px(color='white') mdi-magnify-plus-outline
+          v-btn(icon small @click='scaleReset')
+            v-icon.mdi-24px(color='white') mdi-magnify-close
+          v-btn(icon small @click='scaleOut')
+            v-icon.mdi-24px(color='white') mdi-magnify-minus-outline
+
+        v-card-text.pa-0
+          v-stage.bordered(ref='stage' :config='stageConfig')
+            v-layer(ref='layer' :config='{...scaleConfig}')
+              v-circle(
+                :config='circleConfig'
+                @mouseenter='handleMouseEnter'
+                @mouseleave='handleMouseLeave'
+                @dragstart='handleDragStart'
+                @dragend='handleDragEnd'
+              )
+              UseCase
+            v-layer(ref='dragLayer')
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+<script lang='ts'>
+import Konva from 'konva'
+import { Component, Vue } from 'vue-property-decorator'
+import LayerScaleMixin from '~/mixins/scale-config'
+import ScaleConfigMixin from '~/mixins/cursor-pointer'
+import IPosition from '~/models/Position'
 
-export default {
+let vm: any = {}
+
+@Component({
   components: {
-    Logo,
-    VuetifyLogo
+    UseCase: () => import('~/components/UseCase.vue')
+  },
+  mixins: [LayerScaleMixin, ScaleConfigMixin]
+})
+export default class IndexPage extends Vue {
+  dragStart: IPosition = {
+    x: 0,
+    y: 0
+  }
+  dragEnd: IPosition = {
+    x: 0,
+    y: 0
+  }
+  stageConfig: any = {
+    width: 10,
+    height: 10,
+    draggable: true
+  }
+  circleConfig: any = {
+    x: 35,
+    y: 35,
+    radius: 35,
+    fill: '#00C081',
+    stroke: '#1789C3',
+    strokeWidth: 5,
+    opacity: 0.75,
+    draggable: true,
+    dragBoundFunc: function (pos) {
+      const stage = vm.$refs.stage.getStage()
+      const x1 = 0 + this.width() / 2
+      const y1 = 0 + this.height() / 2
+      const x2 = stage.width() - this.width() / 2
+      const y2 = stage.height() - this.height() / 2
+      const x = (pos.x < x1) ? x1 : (pos.x > x2 ? x2 : pos.x)
+      const y = (pos.y < y1) ? y1 : (pos.y > y2 ? y2 : pos.y)
+      return { x: x, y: y }
+    }
+  }
+
+  created() {
+    vm = this
+  }
+
+  calcStageSize(): void {
+    const editor = document.getElementById('editor')
+    const { clientWidth, clientHeight } = editor as any // TODO!
+    this.stageConfig.width = clientWidth
+    this.stageConfig.height = clientHeight - 74
+  }
+
+  handleDragStart(e): void {
+    const shape = e.target
+    const dragLayer = this.$refs.dragLayer.getNode()
+    const stage = this.$refs.stage.getStage()
+
+    // moving to another layer will improve dragging performance
+    shape.moveTo(dragLayer)
+    stage.draw()
+
+    shape.setAttrs({
+      shadowOffsetX: 15,
+      shadowOffsetY: 15,
+      scaleX: shape.getAttr('scaleX') * 1.2,
+      scaleY: shape.getAttr('scaleY') * 1.2
+    })
+
+    this.dragStart.x = shape.attrs.x.toFixed()
+    this.dragStart.y = shape.attrs.y.toFixed()
+  }
+
+  handleDragEnd(e): void {
+    const shape = e.target
+    const layer = this.$refs.layer.getNode()
+    const stage = this.$refs.stage.getStage()
+
+    // move back to the main layer
+    shape.moveTo(layer)
+    stage.draw()
+
+    shape.to({
+      duration: 0.5,
+      easing: Konva.Easings.ElasticEaseOut,
+      scaleX: shape.getAttr('scaleX') / 1.2,
+      scaleY: shape.getAttr('scaleY') / 1.2,
+      shadowOffsetX: 5,
+      shadowOffsetY: 5
+    })
+
+    this.dragEnd.x = shape.attrs.x.toFixed()
+    this.dragEnd.y = shape.attrs.y.toFixed()
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+ .bordered
+    border 1px dashed grey
+</style>
